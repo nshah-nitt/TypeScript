2d4d6f321de1b815ccdf5d135b9d17ed70ccef8d
Naman Shah <snamanshah@gmail.com>
2025-07-29 00:33:52 +0530

    Initial commit

--- info.ts
+++
@@ -1,6 +0,0 @@
-/**
- * The typescript info
- * 1. The typesctipt is type safe that means we can add type to paramerter or variables
- * 2. The typescript is a superset of javascript that means we can use all the javascript code in typescript
- * 3. The typescript is a compiled language that means we can compile the typescript code to javascript code
- */
