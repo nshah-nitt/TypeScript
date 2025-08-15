35601684070ea372726c1e31d3db2e696f56d3e7
Naman Shah <snamanshah@gmail.com>
2025-08-16 00:21:37 +0530

    folder changes

--- dist/Custom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// when dealing with long complex type , we can use type alias to define a type and use it later
-Object.defineProperty(exports, "__esModule", { value: true });
-let user1 = {
-    name: "Naman",
-    age: 25,
-    hobbies: ["Reading", "Coding"],
-    roles: 'Poc'
-};
--- dist/optional.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// ? is a optional operator 
-function getError(msg) {
-    throw new Error(msg); // This function will never return
-}
