de5e881c1d201304ac2c75542a749cdd7a42fd22
Naman Shah <snamanshah@gmail.com>
2025-08-26 01:35:34 +0530

    added few more methods to linked list

--- dist/linkedList/index.js
+++ dist/linkedList/index.js
@@ -9,14 +9,33 @@ class LinkedList {
     root;
     tail;
     length = 0;
-    add(value) {
+    constructor() {
+        this.root = undefined;
+        this.tail = undefined;
+    }
+    addAttail(value) {
+        // added a new value
         const node = new ListNode(value);
-        if (!this.tail) {
-            this.root = node;
+        if (this.tail) {
+            this.tail.next = node;
             this.tail = node;
         }
         else {
-            this.tail.next = node;
+            // if this is the first node set the root and the tail
+            this.root = node;
+            this.tail = node;
+        }
+        this.length++;
+    }
+    addAtHead(val) {
+        let node = new ListNode(val);
+        if (this.root) {
+            node.next = this.root;
+            this.root = node;
+        }
+        else {
+            // if this is the first node set the root and the tail
+            this.root = node;
             this.tail = node;
         }
         this.length++;
@@ -27,9 +46,40 @@ class LinkedList {
     print() {
         let current = this.root;
         while (current) {
-            console.log(`Current Node has a value ${current.value}\n`);
+            console.log(current.value);
             current = current.next;
         }
     }
+    addAtPos(index, value) {
+        if (index > this.length)
+            return;
+        if (index === 0) {
+            this.addAtHead(value);
+            return;
+        }
+        if (index === this.length) {
+            this.addAttail(value);
+            return;
+        }
+        let node = new ListNode(value);
+        let prev = this.root;
+        for (let i = 0; i < index - 1; i++) {
+            prev = prev?.next;
+        }
+        if (prev) {
+            node.next = prev.next;
+            prev.next = node;
+            this.length++;
+        }
+        return;
+    }
 }
+let linked1 = new LinkedList();
+linked1.addAtHead(1);
+linked1.addAttail(2);
+// linked1.print();
+linked1.addAtPos(1, 9);
+linked1.addAtPos(3, 56);
+// console.log(linked1.getNumberofElements());
+linked1.print();
 export {};
